5491fcb1df65339475b6f1412ffc2a8f74af56df
Ingvi <ithh5@hi.is>
2015-10-16 12:21:19 +0000

    Added working database features using Daníel's implementation

--- src/main/java/project/Application.java
+++ src/main/java/project/Application.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * The main class of the project.
  * By running the main class of {@link Application} then you start the Spring Boot system
  */
+@EnableJpaRepositories
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer{
 
